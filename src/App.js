dbbc9f39cc7304774aedfd8649fbd83cbac45b22
RaveBlue <my-raveblue21@yahoo.com>
2023-06-15 18:24:12 -0600

    updated

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
 //import logo from './logo.svg';
 import "./App.css";
+import React, { useState } from "react";
+//import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+//import Home from "./components/Home";
+//import AboutUs from "./components/AboutUs";
+import Navbar from "./components/Navbar";
+//import Footer from "./components/Footer";
+//import ContactUs from "./components/ContactUs";
+//import Review from "./components/Review";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/AboutUs" element={<AboutUs />} />
+        <Route path="/ContactUs" element={<ContactUs />} />
+        <Route path="/Review" element={<Review />} />
+      </Routes>
+      <Footer />
+    </Router>
   );
 }
+
+<Router>
+  <Route exact path="/" component={Home} />
+  <Route path="/AboutUs" component={AboutUs} />
+  <Route path="/Products" component={Products} />
+  <Route path="/Navbar" component={Navbar} />
+  <Route path="/ContactUs" component={ContactUs} />
+  <Route path="/Review" component={Review} />
+</Router>;
 
 export default App;
---
+++ src/components/Index.js
@@ -0,0 +1,7 @@
+export { default as App } from "../App";
+export { default as Home } from "./Home";
+export { default as Review } from "./Review";
+export { default as Footer } from "./Footer";
+export { default as AboutUs } from "./AboutUs";
+export { default as Navbar } from "./Navbar";
+export { default as ContactUs } from "./ContactUs";
